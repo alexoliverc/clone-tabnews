b6b31ca8f3921bfef3cb98a0dea3597a2c3841b1
Alex oliveira <142450720+alexoliverc@users.noreply.github.com>
2025-09-24 19:04:03 +0000

    adiciona .prettierigone

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Eu sou o cara mais lindo do mundo.</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
